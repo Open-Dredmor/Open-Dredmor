a1fcb8ee1554fd8d0d80b5863a1627665b66e8b9
Tim Kretschmer <xbigtk13x@gmail.com>
2020-07-09 16:02:46 -0500

    Build the main menu programmatically

--- scene/MainMenu/main_menu.gd
+++ scene/MainMenu/main_menu.gd
@@ -1,29 +1,27 @@
 extends Control
 
+var background = null
 var background_image_index = 0
 var background_images = []
 
 func load_background_image():	
-	$Background.texture = background_images[background_image_index]
-	$Background.rect_min_size = Vector2($Background.texture.get_width(), $Background.texture.get_height())
+	background.texture = background_images[background_image_index]
+	background.rect_min_size = Vector2(background.texture.get_width(), background.texture.get_height())
 
 func _ready():
-	background_images = Assets.main_menu_backgrounds()
+	var main_menu = get_tree().get_root()
+	background_images = Assets.main_menu_backgrounds()	
+	background = TextureRect.new()
 	
-	load_background_image()
+	main_menu.add_child(background)	
+	load_background_image()	
 	
-	var menu_chrome = $Background/MenuChrome
+	var menu_chrome = TextureRect.new()
 	menu_chrome.texture = Assets.main_menu_chrome()
 	menu_chrome.rect_min_size = Vector2(menu_chrome.texture.get_width(), menu_chrome.texture.get_height())
 	menu_chrome.anchor_left = .8
-	menu_chrome.anchor_top = .1
-		
-	var background_button = $Background/MenuChrome/BackgroundButton
-	Chrome.button(background_button, Assets.main_menu_background_button())
-	background_button.anchor_left = .5
-	background_button.anchor_top = .89
-	background_button.margin_left = -(background_button.texture_normal.get_width()/2)
-	background_button.margin_top = -(background_button.texture_normal.get_height()/2)	
+	menu_chrome.anchor_top = .1	
+	background.add_child(menu_chrome)
 	
 	var quit_button = TextureButton.new()
 	Chrome.button(quit_button, Assets.main_menu_quit_button())
@@ -34,9 +32,19 @@ func _ready():
 	quit_button.connect("pressed",self,"_on_QuitButton_pressed")
 	menu_chrome.add_child(quit_button)
 	
-	$MusicPlayer.stream = Assets.main_menu_music()
-	$MusicPlayer.play()
+	var background_button = TextureButton.new()
+	Chrome.button(background_button, Assets.main_menu_background_button())
+	background_button.anchor_left = .5
+	background_button.anchor_top = .89
+	background_button.margin_left = -(background_button.texture_normal.get_width()/2)
+	background_button.margin_top = -(background_button.texture_normal.get_height()/2)	
+	background_button.connect("pressed", self, "_on_BackgroundButton_pressed")
+	menu_chrome.add_child(background_button)
 	
+	var music_player = AudioStreamPlayer.new()	
+	music_player.stream = Assets.main_menu_music()
+	music_player.play()
+	main_menu.add_child(music_player)
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
